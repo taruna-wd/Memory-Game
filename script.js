eddc598eed7ce154a91604e8eb0ee8db5e2e78d6
taruna-wd <tarunarajoriya18@gmail.com>
2024-07-11 15:47:17 +0530

    memory game

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
-const cards  = document.querySelectorAll(".card-outer")
-const AllImage = document.querySelectorAll(".card-front img")
-// console.log(AllImage)
 
-let firstMove , secondMove;
+const cards = document.querySelectorAll(".card-outer")
+const btn = document.querySelector(".scoreBtn")
 
-cards.forEach(card => card.addEventListener("click" , ()=>{
-if(card !== firstMove){
-    card.classList.toggle('flip')
-    if(!firstMove){
-        return firstMove = card
+let firstCard, secondCard;
+let board = true
+score = 0
+
+cards.forEach(card => card.addEventListener("click", () => {
+    btn.innerText = score
+
+    if (!board) return; // Do nothing if the board is unlocked
+    if (card == firstCard) return; // no action if clicked same card
+
+    card.classList.add("flip") // flip the card
+
+    if (!firstCard) {
+        firstCard = card;
+        console.log(firstCard);
+        return;
     }
-    // firstMove = card;
-    secondMove =card
-    let img1 = firstMove.querySelector("")
-    console.log(secondMove , firstMove) 
-}
-  
+
+    secondCard = card // if set already firstcard then clicked second card
+    console.log(secondCard)
+
+    matchCards()
 }))
 
 
+function matchCards() {
+    firstImg = firstCard.querySelector(".card-front img").alt 
+    secondImg = secondCard.querySelector(".card-front img").alt
+
+    if (firstImg == secondImg) { // if match same alt text
+        reset()
+        score = score + 2
+        if (score == 12) {
+            congratulationsBox()
+        }
+    } else {
+        board = false
+        setTimeout(() => {
+            firstCard.classList.remove("flip")
+            secondCard.classList.remove("flip")
+            reset()
+        }, 1000)
+    }
+
+}
+
+function congratulationsBox() {
+
+    const congratsBox = document.getElementById('congratulations-box');
+    congratsBox.classList.remove('hidden');
+
+    const playAgainButton = document.getElementById('play-again');
+    playAgainButton.addEventListener('click', () => {
+        congratsBox.classList.add('hidden');
+        setTimeout(() => {
+            cards.forEach(card => card.classList.remove("flip"))
+            btn.innerHTML = '0'
+        }, 1000)
+    });
+}
+
+function reset() {
+    [firstCard, secondCard] = [null, null]
+    board = true
+}
